f726852847eaa62d42ded75a76b92788e293e453
noor sawaed <n.swa3ed.swa3ed@gmail.com>
2022-07-30 22:50:22 +0300

    started aal over again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from "../src/components/Navbar/Navbar";
+
 
 function App() {
-  return (<div>
-    <Navbar></Navbar>
-    <h1>hello world!</h1>
+  return (<div className="App">
+    
+
     </div>
   );
 }
